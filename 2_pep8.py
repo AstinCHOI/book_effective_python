12a5e279f7046fc8d048a562a35e0b9b31362dba
astinchoi <asciineo@gmail.com>
2016-06-01 08:46:33 +0900

    better way 2

---
+++ 2_pep8.py
@@ -0,0 +1,17 @@
+# PEP(Python Enhancement Proposal) 8
+
+# White space
+# Under 79 characters for a line
+# 2 empty lines for function and class / a line for method in the class
+
+# Naming
+# Function, variable and property use "lowercase_underscore"
+# protected instance property follows "_leading_underscrore"
+# private instance property does "__double_leading_underscore"
+# Class and Exception does CaptalizedWord
+# Module level constant does ALL_CAPS
+# Class instance: self & Class method: cls for the first param
+
+# Expression and Statement
+# Use if a is not b instead if not a is b
+# ...
