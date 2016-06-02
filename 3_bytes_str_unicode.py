4d50218a5eccdcd0d600b74862364aa3939f3022
astinchoi <asciineo@gmail.com>
2016-06-02 23:57:42 +0900

    bytes, str, unicode

---
+++ 3_bytes_str_unicode.py
@@ -0,0 +1,8 @@
+
+# python3
+# character sequence: bytes(raw 8 bit), str(unicode)
+
+# python2
+# character sequence: str(raw 8 bit), unicode(unicode)
+
+# unicode to binary: encode, vice versa decode
